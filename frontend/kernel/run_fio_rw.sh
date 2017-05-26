95c383b54bad7e8e83ec18f2aee734e073bff2bf
JHOONL <jhoonl89@gmail.com>
2017-05-26 17:12:43 +0900

    first commit

---
+++ frontend/kernel/run_fio_rw.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo rm /media/robusta/fio
+
+sudo fio --randrepeat=1 \
+	--ioengine=libaio \
+	--name=fio\
+	--filename=/media/robusta/fio \
+	--bs=4k \
+	--iodepth=128 \
+	--size=400M \
+	--readwrite=randrw \
+	--rwmixread=0 \
+	--overwrite=1 \
+	--numjobs=1 \
+	--direct=0 \
+	--buffered=0
+
